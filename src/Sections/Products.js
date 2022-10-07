503ca3d162fbfd0df294952a8f4f6cb2ad3e8400
Swapnil1296 <swapnillandage79@gmail.com>
2022-10-07 18:34:25 +0530

    debouncing adde

--- src/Sections/Products.js
+++ src/Sections/Products.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-import React, { useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import React, { useCallback, useEffect, useState } from "react";
+import { Col, Form, InputGroup, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 const Products = ({
   divCode,
   dist_id,
   cfaNumber,
-  
+
   disableThree,
   valueForHolder,
   disabledOther,
@@ -16,12 +16,17 @@ const Products = ({
 }) => {
   // console.log("data in Products page:-", divCode, dist_id, cfaNumber);
   const [data, setData] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  console.log("inputValue:-", inputValue);
+  const handleValue = (e) => {
+    setInputValue(e.target.value);
+  };
   useEffect(() => {
     if (disabledOther || disableThree || clearProThree) {
       setData("");
     }
   }, [disabledOther, disableThree, clearProThree]);
- 
+
   const Token = useSelector((state) => state.Auth);
   const handleData = () => {
     axios
@@ -39,19 +44,56 @@ const Products = ({
         console.log(error);
       });
   };
- useEffect(() => {
-   console.log("api hit");
-   if (valueForHolder !== "") {
-     handleData();
+  const searchData = (value = "", cfaNumber, dist_id, divCode) => {
+    axios
+      .get(
+        `https://alkemapi.indusnettechnologies.com/api/product/all_product_list/E/${cfaNumber}?dist_id=${dist_id}&page=${1}&sv=&div_code=${divCode}&product_nm=${value}`,
+        {
+          headers: { Authorization: `Bearer ${Token}` },
+        }
+      )
+      .then(function (response) {
+        // console.log("response data in Products Page:-", response.data.data);
+        setData(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const debounce = (fn, delay) => {
+    // console.log(fn)
+    let timerId;
+    return (...args) => {
+      // console.log(args);
+      timerId && clearTimeout(timerId);
+      timerId = setTimeout(() => fn(...args), delay);
+    };
+  };
+  const debouncedHandler = useCallback(debounce(searchData, 1500), []);
+
+  useEffect(() => {
+    console.log("api hit");
+    if (valueForHolder !== "") {
+      handleData();
       console.log("api hit again");
-   }
-   if(valueForHolder===''){
-    setData('')
-   }
- }, [valueForHolder,divCode]);
+    }
+    if (valueForHolder === "") {
+      setData("");
+    }
+  }, [valueForHolder, divCode]);
 
   return (
     <>
+      <InputGroup className="mb-3" style={{ width: "300px", margin: "5px" }}>
+        <Form.Control
+          placeholder="Search Product/Code/Composition"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+            debouncedHandler(e.target.value, cfaNumber, dist_id, divCode);
+          }}
+        />
+      </InputGroup>
       {data &&
         data.map((item, i) => (
           <Row className="border border-primary m-2" key={i}>
--- src/Sections/SearchSection.js
+++ src/Sections/SearchSection.js
@@ -2,17 +2,10 @@ import React, { useRef, useState } from "react";
 import { InputGroup, Form } from "react-bootstrap";
 
 const SearchSection = () => {
-    const [inputValue, setInputValue] = useState("");
-  console.log("inputValue:-", inputValue);
+  
   return (
     <>
-      <InputGroup className="mb-3" style={{ width: "300px", margin: "5px" }}>
-        <Form.Control
-          placeholder="Search Product/Code/Composition"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-      </InputGroup>
+    
     </>
   );
 };
